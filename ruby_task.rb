85a4ca276673b1f6306fcefdb35671f23a39d32d
Md Azharuddin <mohammad.azhar2000@gmail.com>
2016-03-23 01:17:50 +0530

    Update ruby_task.rb

--- ruby_task.rb
+++ ruby_task.rb
@@ -8,40 +8,36 @@ def manage_load(arr)
     return
   end
   balanced_load = arr.inject(:+)/arr.size
-  (arr.min+1..balanced_load).each do |bal_ld|
   begin
     arr.each_with_index do |load, indx|
-      distribute_load(arr,indx, bal_ld) if load > bal_ld
+      distribute_load(arr,indx, balanced_load) if load > balanced_load
     end
     t += 1
-    #puts "Server loads at time t=#{t}"
-    #puts arr.join(",")
-  end while arr.any? {|ser_ld| ser_ld < bal_ld}
-  
-  end
+    puts "Server loads at time t=#{t}"
+    puts arr.join(",")
+  end while arr.any? {|ser_ld| ser_ld < balanced_load}
   puts t
 end
   
 def distribute_load(a, i, bal)
   l = a.length
   j,k= i,i
-  while (a[j-1] != (bal-1) and j>1)
+  while (a[j-1] and a[j-1] >= bal and j>1)
       j-=1
   end
-  if a[j-1] == bal-1 and a[i] >bal and i!=0
+  if a[j-1] and a[j-1] < bal and a[i] >bal and i!=0
       a[j-1]+=1
       a[i]-=1
   end
-  while (a[k+1] != bal-1 and k<l)
+  while (a[k+1] and a[k+1] >= bal and k<l)
       k+=1
   end
-  if a[k+1] == bal-1 and a[i]>bal
+  if a[k+1] and a[k+1] < bal and a[i]>bal
       a[k+1]+=1
       a[i]-=1
   end 
 end
 
-    
 puts "Enter the Input"
 input=gets.split(",")
 invalid_ld = 0
